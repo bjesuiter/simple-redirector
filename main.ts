0755dfcbb466d77540e70a4672e4b4e172763fc7
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-01-26 11:52:04 +0100

    switch to console.log

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 const targetUrl = Deno.env.get("TARGET_URL");
 
 if (!targetUrl) {
-  console.error("TARGET_URL environment variable is not set");
+  console.log("TARGET_URL environment variable is not set");
 } else {
   const handler = (_request: Request, _info: any) => {
     return new Response(undefined, {
