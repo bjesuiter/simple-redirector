dfba05ba347f983e150501418f626cdd71407a66
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-01-26 11:51:11 +0100

    make sure main.ts runns gracefully, otherwise deno deplyo setup is not possible

--- main.ts
+++ main.ts
@@ -2,16 +2,15 @@ const targetUrl = Deno.env.get("TARGET_URL");
 
 if (!targetUrl) {
   console.error("TARGET_URL environment variable is not set");
-  Deno.exit(0);
+} else {
+  const handler = (_request: Request, _info: any) => {
+    return new Response(undefined, {
+      status: 301,
+      headers: {
+        location: targetUrl,
+      },
+    });
+  };
+
+  Deno.serve(handler);
 }
-
-const handler = (_request: Request, _info: any) => {
-  return new Response(undefined, {
-    status: 301,
-    headers: {
-      location: targetUrl,
-    },
-  });
-};
-
-Deno.serve(handler);
